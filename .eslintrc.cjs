9c8196624630597e9286a40b95c9282a97154016
Zyy <No email>
2022-10-12 02:04:33 +0800

    1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,46 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true
+  },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
-    'unplugin-auto-import/.eslintrc-auto-import.json'
+    './node_modules/unplugin-auto-import/.eslintrc-auto-import.json'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
+  plugins: ['import'],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
     'prettier/prettier': 'warn',
 
+    // vue
     'vue/attributes-order': 'error',
     'vue/order-in-components': 'error',
-    'vue/multi-word-component-names': 0
+    'vue/multi-word-component-names': 'off',
+    'vue/eqeqeq': ['error', 'always'],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style']
+      }
+    ],
+
+    // import
+    'import/order': 'error',
+    'import/first': 'error',
+    'import/no-mutable-exports': 'error',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off'
   }
 }
